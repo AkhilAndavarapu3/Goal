0c1351db80763e2b71c25f61a842b41612840c24
Akhil <akhilandavarapu@gmail.com>
2021-06-12 11:51:16 +0530

    Modified jenkins file

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,7 +1,18 @@
 pipeline {
 	agent any 
+	environment{
+		NEW_METHOD == 'overriding'
+	}
+	parameters{
+        choice(name:'VERSION',choices:['1.2','1.3','1.4'],description:'For choosing choice')
+	}
 		stages {
 			stage('ONE') {
+				when{
+					expression{
+						BRANCH_NAME == 'main'
+					}
+				}
 				steps {
 					echo "Hi ALL, Let's start the jenkins pipeline"
 				}
@@ -29,6 +40,20 @@ pipeline {
 
 					
 				}
+			}
+			stage('FOUR'){
+				steps{
+					echo "new version is ${params.VERSION}"
+					echo "new method is ${NEW_METHOD}"
+				}
+			}
+		}
+		post{
+			success{
+				echo "Build was successfull"
+			}
+			failure{
+				echo "Build Failed"
 			}
 		}
 
